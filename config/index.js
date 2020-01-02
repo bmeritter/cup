430a4a5b92bd52fef9089bc76065ca6a09ac606c
Qiqi Zhao <qiqzhao@thoughtworks.com>
2020-01-02 17:25:28 +0800

    feat: gallery

--- config/index.js
+++ config/index.js
@@ -1,8 +1,9 @@
 const config = {
   projectName: 'cup-shop',
   date: '2020-1-2',
-  designWidth: 750,
+  designWidth: 375,
   deviceRatio: {
+    '375': 1 / 2,
     '640': 2.34 / 2,
     '750': 1,
     '828': 1.81 / 2
--- src/pages/index/gallery.tsx
+++ src/pages/index/gallery.tsx
@@ -1,11 +1,45 @@
 import Taro, { Component } from '@tarojs/taro';
-import { View } from '@tarojs/components';
+import { ScrollView, Text, View } from '@tarojs/components';
+import './gallery.scss';
+
+const galleries = [
+  {
+    id: 1,
+    name: '商品1',
+    price: 100,
+  }, {
+    id: 2,
+    name: '商品2',
+    price: 100,
+  }, {
+    id: 3,
+    name: '商品3',
+    price: 100,
+  }
+];
 
 export default class Gallery extends Component {
+  onScroll = () => {
+    console.log('scroll')
+  };
+
   render() {
     return (
       <View>
-        Gallery
+        <View className='title'>
+          <Text>本期推荐</Text>
+          <Text>1/{galleries.length}</Text>
+        </View>
+        <ScrollView
+          className='gallery'
+          scrollX
+          scrollWithAnimation
+          onScroll={this.onScroll}
+        >
+          {
+            galleries.map(item => (<View key={item.id}>{item.name}</View>))
+          }
+        </ScrollView>
       </View>
     );
   }
